8d8b1597d9f6a9bde498b747ed2a8acd9ac0dd0c
Nadeem <nadeembinnoushad12@gmail.com>
2021-05-17 03:31:51 +0530

    idk

--- modules/help.js
+++ modules/help.js
@@ -1,14 +1,38 @@
-module.exports = (client, prefix) => {
+module.exports = (client, prefix, embed) => {
     client.on('message', message => {
         if (message.content === `${prefix}help`) {
-                message.channel.send(
-`All the commands for this bot are down belown
-prefix: '!'
-help -> displays help
-apis -> displays all the states and state_id
-apid -> displays all the districts and district_id
-reg -> users can register vaccination appoinment using their district and age group as args`
-)
-        }
+            embed
+                .setTitle("COWIN BOT HELP")
+                .setColor("#6969")
+                .setFooter("Stay home and wear a mask")
+                .addFields(
+                    {
+                        name: '!help',
+                        value: 'Displays help'
+                    },
+                    {
+                        name: '!get_states',
+                        value: 'Displays all states and corresponding state id'
+                    },
+                    {
+                        name: '!get_districts [state_id]',
+                        value: 'Displays all districts and corresponding district id'
+                    },
+                    {
+                        name: '!reg [district_id] [age_group]',
+                        value: 'Register for cowin updates using district and age group'
+                        
+                    }
+                )
+            
+            message.channel.send(embed)
+        }// end of if
+
+        client.user.setPresence({
+            activity: {
+                name: `"${prefix}help" for help`
+            }
+        })        
     })
+
 }
