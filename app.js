ab35265692bed2d79f87f620508b41db56fc1150
Nadeem <nadeembinnoushad12@gmail.com>
2021-05-17 03:31:21 +0530

    updated few stuffs ig

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-const { Client } = require('discord.js')
+const { Client, MessageEmbed } = require('discord.js')
 const fetch = require('node-fetch')
+const mongoose = require('mongoose')
+
 const help = require('./modules/help')
-const api = require('./api/api')
+const api = require('./api')
+const User = require('./model/Users')
 
 require('dotenv').config()
-
 
 const prefix = '!'
 
 // New Discord Client
 const client = new Client()
+const embed = new MessageEmbed()
 
 client.on('ready', () => {
     console.log("BOT IS UP AND RUNNING...!")
 })
 
-// listening to all msgs
+// DB Setup 
+const db = process.env.DBKEY
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection.once('open', () => console.log('Connection made...'))
 
-help(client, prefix)
+
+
+// listening to all msgs
+client.on('message', message => {
+    // Dumby code; for testing lol
+
+    if(!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).trim().split(' ')
+    const command = args.shift().toLowerCase()
+
+    if (command === 'reg') {
+        console.log(message.author.username)
+        console.log(message.author.id)
+        console.log(message.author.client)
+        console.log(message.author.tag)
+    }
+})
+
+help(client, prefix, embed)
 
 // API CALLS
 
