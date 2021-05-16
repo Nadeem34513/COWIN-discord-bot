b34716e2f8c877b640c3236746e5f6165d091461
Nadeem <nadeembinnoushad12@gmail.com>
2021-05-17 03:29:43 +0530

    idk how to explain, hehe

---
+++ api.js
@@ -0,0 +1,71 @@
+const fetch = require('node-fetch')
+
+const UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0' 
+
+//get states 
+const get_states = (client, prefix) => {
+    // function not done yet
+    client.on('message', async message => {
+        const args = message.content.slice(prefix.length).trim().split(' ')
+        const command = args.shift().toLowerCase()
+
+        if(message.content.startsWith(prefix) && command === 'api' && args[0] === 'get_state'){
+            message.channel.send('heres the api response(s)')
+                    
+            const data = await fetch('https://cdn-api.co-vin.in/api/v2/admin/location/states', {
+                // imp
+                headers: {
+                    'User-Agent': UserAgent
+                }
+            }).then(response => response.json())
+              .catch(err => console.log(err))
+                
+            const newData = JSON.stringify(data)
+            // console.log(newData)
+            // newData.replace(/{|}|"|/g,' ')
+            // console.log(typeof(newData))
+            // console.log(typeof(data))
+            // message.channel.send(newData)
+            message.channel.send(data.states[17].state_name)
+            
+        }
+    
+    })
+}
+
+// get district
+const get_district = (client, prefix) => {
+    // function not done yet
+    client.on('message', async message => {
+        const args = message.content.slice(prefix.length).trim().split(' ')
+        const command = args.shift().toLowerCase()
+
+        if(message.content.startsWith(prefix) && command === 'api' && args[0] === 'get_district'){
+            message.channel.send('heres the api response(s)')
+                    
+            const data = await fetch(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${args[1]}`, {
+                // imp
+                headers: {
+                    'User-Agent': UserAgent
+                }
+            }).then(response => response.json())
+            
+            
+            console.log(data)
+            
+            // await message.channel.send(data)
+        }
+    
+    })
+}
+
+
+
+
+
+
+
+module.exports =  {
+    get_states,
+    get_district
+}
