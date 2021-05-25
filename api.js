5678edd609372f56743f22c50c4f5e87b51f151c
Nadeem Noushad <65117082+Nadeem34513@users.noreply.github.com>
2021-05-26 02:13:24 +0530

    cleaned up the code

--- api.js
+++ api.js
@@ -1,114 +1,62 @@
 const fetch = require('node-fetch')
 const HttpsProxyAgent = require('https-proxy-agent')
 const axios = require('axios')
+const { model } = require('mongoose')
 
 const UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0' 
 const baseUrl = 'https://cowin.rabeeh.me/api'
 
-//get states 
-const get_states = (client, prefix) => {
-    // function not done yet
-    client.on('message', async message => {
+
+module.exports = (client, prefix) => {
+    client.on('message', message => {
         const args = message.content.slice(prefix.length).trim().split(' ')
         const command = args.shift().toLowerCase()
 
-        if(message.content.startsWith(prefix) && command === 'get_states'){
-            message.channel.send('check the console for api response(s)')
-            
-            const proxyAgent = new HttpsProxyAgent('http://46.250.171.31:8080')
-            const data = await fetch(`${baseUrl}/v2/admin/location/states`, {
-                headers: {
-                    agent: proxyAgent,
-                    'User-Agent': UserAgent
-                }
-            }).then(response => response.json())
-              .catch(err => console.log(err))
-                
-            console.log(data)
-            let newData = JSON.stringify(data)
-        //    newData=newData.replace("{", ' ');
-        //    newData=newData.replace("}", ' ');
-        //    newData=newData.replace("\"", ' ');
-        //    newData=newData.replace(",", ' ');
-        //     newData = newData.substr(0, newData.length - 1);
-            //newData = newData.replace(/{|}|"|,/g,'')
-            newData = newData.replace(/{|}|"/g,'')
-            newData = newData.replace(/,/g,'\n')
-            newData = newData.replace(/[|]/g,'\n')
-
-            // { ,}, and " gets replaced by a blank space
-            console.log(newData)
-            message.channel.send(newData)
-            
+        // get_states function
+        if (message.content.startsWith(prefix) && command === 'get_states') {
+            get_states(message)    
         }
+        else if (message.content.startsWith(prefix) && command === 'get_district') {
+            get_district(message, args)
+        }        
     })
 }
 
-// get district
-const get_district = (client, prefix) => {
-    // function not done yet
-    client.on('message', async message => {
-        const args = message.content.slice(prefix.length).trim().split(' ')
-        const command = args.shift().toLowerCase()
-
-        if(message.content.startsWith(prefix) && command === 'get_districts'){
-            message.channel.send('check the console for the api response(s)')
-            
-            const proxyAgent = new HttpsProxyAgent('http://46.250.171.31:8080')
-            const data = await fetch(`${baseUrl}/v2/admin/location/districts/${args[0]}`, {
-                // imp
-                //agent: proxyAgent,
-                headers: {
-                    'User-Agent': UserAgent
-                }
-            }).then(response => response.json())
-            
-            
-            console.log(data)
-            let newData = JSON.stringify(data)
-
-            newData = newData.replace(/{|}|"/g,'')
-            newData = newData.replace(/,/g,'\n')
-
-            message.channel.send(newData)
+const get_states = async (message) => {
+    message.channel.send('check the console for api response(s):cry:')
+    
+    //const proxyAgent = new HttpsProxyAgent('http://46.250.171.31:8080')
+    const data = await fetch(`${baseUrl}/v2/admin/location/states`, {
+        headers: {
+            //agent: proxyAgent,
+            'User-Agent': UserAgent
         }
-        // discord edukk
-    })
+    }).then(response => response.json())
+      .catch(err => console.log(err))
+        
+    console.log(data)
+    let newData = JSON.stringify(data)
+    newData = newData.replace(/{|}|"/g,'')
+    newData = newData.replace(/,/g,'\n')
+    newData = newData.replace(/[|]/g,'\n')
+
+    message.channel.send(newData)
+}           
+
+
+const get_district = async (message, args) => {    
+
+    const data = await fetch(`${baseUrl}/v2/admin/location/districts/${args[0]}`, {
+        //agent: proxyAgent,
+        headers: {
+            'User-Agent': UserAgent
+        }
+    }).then(response => response.json())
+
+    console.log(data)
+    let newData = JSON.stringify(data)
+    newData = newData.replace(/{|}|"/g,'')
+    newData = newData.replace(/,/g,'\n')
+    message.channel.send(newData)
+       
 }
-
-module.exports =  {
-    get_states,
-    get_district
-}
-
-
-
-// function replaceLastCharacter() {
-//     var str = 'tracedynamics';
-//     str = str.replace('s','S');
-//     console.log(str); 
-// }
-    
-// // //str.substr()
-// function removeLastCharacter() {
-//     var str = ' { " states " : [ { " state_id ": 1, " state_name " : " Andaman and Nicobar Islands " } , { " state_id " : 2,';
-//     str = str.replace("{", ' ').replace("}", ' ').replace("[", ' ').replace("]", ' ')
-
-//     console.log(str); 
-// }
-    
-// removeLastCharacter()
-// // newString = string.replace(/\s+/g,' ').trim();
-
-
-
-// function removeFirstCharacter() {
-//     var str = 'tracedynamics hello';
-//     str = str.slice(0,str.length-1);
-//     console.log(str); 
-//     }
-
-    
-// removeFirstCharacter()
-
-// //var stringWithoutLineBreaks = stringWithLineBreaks.replace(/(\r\n|\n|\r)/gm, "");
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const User = require('./model/Users')
 const { Client, MessageEmbed } = require('discord.js')
 const mongoose = require('mongoose')
-const axios = require('axios').default
 
 const help = require('./commands/help')
 const reg = require('./commands/reg')
 const update = require('./commands/update')
+const hourlyUpdate = require('./commands/hourlyUpdate')
 const api = require('./api')
-
 
 require('dotenv').config()
 
@@ -15,6 +13,7 @@ const prefix = process.env.PREFIX
 
 // New Discord Client
 const client = new Client()
+
 const embed = new MessageEmbed()
 
 client.on('ready', () => {
@@ -26,27 +25,13 @@ const db = process.env.DBKEY
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 mongoose.connection.once('open', () => console.log('Connection made...'))
 
-
-// listening to all msgs
-client.on('message', async message => {
-    //Dumby code; for testing lol
-    if (message.content.startsWith(prefix) && message.content.includes('axiosCall')) {
-        const baseUrl = 'https://cdn-api.co-vin.in/api'
-        console.log('hello')
-        const data = await axios.get(`${baseUrl}/v2/admin/location/states`).then(res => console.log(res))
-        console.log(data)
-    }
-})
-
-// Commands with prefix: '!'
+// Calling Commands
 help(client, prefix, embed)
 reg(client, prefix)
 update(client, prefix)
+hourlyUpdate(client, prefix)
 
-// API CALLS
-
-api.get_states(client, prefix)
-api.get_district(client, prefix)
+api(client, prefix)
 
 // Login
 client.login(process.env.BOT_TOKEN)
